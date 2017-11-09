744754fab78d1d6dcf93e3ed96bb2a3d488832a4
oauger <olivier.auger@gmail.com>
2017-11-09 17:07:28 +0100

    test échec compilation

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -30,7 +30,7 @@ public class PetClinicApplication {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PetClinicApplication.class, args);
-
+        xxx
 	//TODO test :)
     }
 
