d5193dfd7b570489c1b74e5d6964677ed986d2e7
x2i-tvercruysse <tvercruysse@clever-age.com>
2018-09-21 11:11:27 +0200

    Update CrashController.java

--- src/main/java/org/springframework/samples/petclinic/system/CrashController.java
+++ src/main/java/org/springframework/samples/petclinic/system/CrashController.java
@@ -15,7 +15,7 @@
  */
 package org.springframework.samples.petclinic.system;
 
-import org.springframework.stereotype.Controllerze;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
