d3ab1364557e2eeb543090ac6649a5484283eaa1
x2i-tvercruysse <tvercruysse@clever-age.com>
2019-01-24 16:15:56 +0100

    Update ValidatorTests.java

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -40,8 +40,7 @@ public class ValidatorTests {
         assertThat(constraintViolations.size()).isEqualTo(1);
         ConstraintViolation<Person> violation = constraintViolations.iterator().next();
         assertThat(violation.getPropertyPath().toString()).isEqualTo("firstName");
-        //assertThat(violation.getMessage()).isEqualTo("may not be empty");
-        assertThat(violation.getMessage()).isEqualTo("");
+        assertThat(violation.getMessage()).isEqualTo("may not be empty");
     }
 
 }
