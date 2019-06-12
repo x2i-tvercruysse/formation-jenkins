41c9ce3df188c8559aba48cec9160a9fcbf5d324
x2i-tvercruysse <tvercruysse@clever-age.com>
2019-06-12 12:01:11 +0200

    Update VetController.java

--- src/main/java/org/springframework/samples/petclinic/vet/VetController.java
+++ src/main/java/org/springframework/samples/petclinic/vet/VetController.java
@@ -30,7 +30,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 class VetController {
-
+    //ORSYSTODO rename property
     private final VetRepository vets;
 
     @Autowired
